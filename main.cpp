b894eb225c672e7662465b68bb907453957eb6e6
mayakovskyj <mashunya.korol@yandex.ru>
2017-10-31 01:34:18 +0200

    Update main.cpp
    Checking karatsuba and toom3 multiplications on two big numbers. Results are identical.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,14 @@
 using namespace std;
 
 int main() {
-	long_number myNumber = long_number("12345678911234567891");
-	long_number myNumber1 = long_number("148999879879997979797799");
-	long_number n1 = long_number("146979677678");
-	long_number n2 = long_number("911246913569");
-	n1 = n1 * n2;
-	//n1.theNumber();
-	myNumber = myNumber.karatsuba(myNumber1);
-	//myNumber = myNumber.toom3wayMultiplication(myNumber1);
-//	myNumber = myNumber1 - myNumber;
-//	cout << result.theNumber() << endl;
-//	myNumber.theNumber();
-//	myNumber1.theNumber();
+	long_number myNumber;
+	long_number myNumber1;
+	long_number b1("194305406543241241242131231249");
+	long_number lom("991948532142142131231094755497");
+	myNumber = b1.toom3wayMultiplication(lom);
+	myNumber1 = lom.karatsuba(b1);
 	myNumber.theNumber();
+	myNumber1.theNumber();
 	system("pause");
 	return 0;
 }
