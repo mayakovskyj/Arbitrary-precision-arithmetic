1cf74ec21ace9f7f66c61bee9d7d2091ca76833f
mayakovskyj <mashunya.korol@yandex.ru>
2017-11-03 00:14:34 +0200

    Update long_number.h
    Updated all files
    Added Shoenhage and Strassen multiplication methods. 
    Testing 4 methods in main.cpp

--- long_number.h
+++ long_number.h
@@ -2,9 +2,16 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <complex> //using for FFT
 
+using std::complex;
 using std::string;
 using std::vector;
+using std::iterator;
+
+#define PI           3.14159265358979323846
 
 class long_number
 {
@@ -14,19 +21,30 @@ public:
 	long_number();
 	long_number(const string s);
 	long_number(vector<int> theNumber);
+	long_number(const long long &x);
 
-	//Ha-ha, classic algorithms
-	const long_number operator*(const long_number& other)const;
+	//operators
 	const long_number operator+(const long_number& other)const;
 	const long_number operator-(const long_number& other)const;
-	bool operator>(const long_number& other)const;
-	const long_number operator / (const int &x)const;
+	const long_number operator*(const long_number& other)const;
+	const long_number operator / (const long long &x)const;
+	const long_number operator / (const long_number& other)const;
+	const long_number operator%(const long long &x)const;
+	const long_number operator%(const long_number &other)const;
+
 	long_number& operator=(const long_number& right);
-	//Ha-ha, no ha-ha :|
+	bool operator>(const long_number& other)const;
+	bool operator==(const long_number& other)const;
+
+	//Multiplication algorithms 
 	long_number karatsuba(long_number& other);
 	long_number toom3wayMultiplication(long_number& other);
-//	long_number toomInverse(long_number& other);
+	long_number shoenhage(long_number& other);
+		int bits_in_number()const;
+		long_number find_inverse_by_mod(long_number& m) const;
+	long_number strassen(long_number& other);
+		void FFT(vector<complex<double>> &a, bool invert);
+
 	void theNumber()const;
 	~long_number();
 };
-
